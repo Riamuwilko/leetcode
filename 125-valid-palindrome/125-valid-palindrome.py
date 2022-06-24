31332642154071df1a3a92bb73a79e2a9bc2f7b1
Riamuwilko <94025571+Riamuwilko@users.noreply.github.com>
2022-06-24 13:30:25 -0400

    Time: 69 ms (54.56%), Space: 14.4 MB (85.60%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ans = ""
+        for c in s:
+            if c.isalnum():
+                ans += c.lower()
+        return ans[::-1] == ans
