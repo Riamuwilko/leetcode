87b93cbf8364d6549ce3044570c5b03993d6f361
Riamuwilko <wilko@umich.edu>
2022-10-18 17:14:48 -0400

    Time: 98 ms (88.13%), Space: 15.7 MB (20.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -5,8 +5,6 @@ class Solution:
         while(scanner < len(nums)):
             if nums[scanner-1] != nums[scanner]:
                 nums[count] = nums[scanner]
-                count+=1
-                scanner+=1
-            else:
-                scanner+=1
+                count+=1    
+            scanner+=1
         return count
